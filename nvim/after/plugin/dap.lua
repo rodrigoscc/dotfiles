821892a6b1baa302fa33030492be83e4cae8d152
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2023-04-04 22:54:31 -0500

    Customize dap signs and add breakpoint keymaps.

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -2,6 +2,17 @@ local dap = require("dap")
 local dapui = require("dapui")
 
 vim.keymap.set("n", "<leader>tb", dap.toggle_breakpoint, { desc = "[t]oggle [b]reakpoint" })
+vim.keymap.set("n", "<leader>bL", function()
+	vim.ui.input({ prompt = "Log breakpoint message:" }, function(input)
+		dap.set_breakpoint(nil, nil, input)
+	end)
+end, { desc = "[b]reakpoing [l]og message" })
+vim.keymap.set("n", "<leader>bc", function()
+	vim.ui.input({ prompt = "Condition:" }, function(input)
+		dap.set_breakpoint(input, nil, nil)
+	end)
+end, { desc = "[b]reakpoing [c]ondition" })
+
 vim.keymap.set("n", "<leader>td", dapui.toggle, { desc = "[t]oggle [d]apui" })
 
 vim.keymap.set("n", "<leader>cb", dap.clear_breakpoints, { desc = "[c]lear [b]reakpoints" })
@@ -15,3 +26,17 @@ vim.keymap.set("n", "<leader>si", dap.step_into, { desc = "[s]tep [i]nto" })
 vim.keymap.set("n", "<leader>su", dap.step_out, { desc = "[s]tep o[u]t" })
 
 vim.keymap.set("n", "<leader>de", dapui.eval, { desc = "[d]ebug [e]val" })
+
+vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939" })
+vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef" })
+vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379" })
+vim.api.nvim_set_hl(0, "DapStoppedLine", { ctermbg = 0, bg = "#31353f" })
+
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint", numhl = "DapBreakpoint" })
+vim.fn.sign_define("DapBreakpointCondition", { text = "ﳁ", texthl = "DapBreakpoint", numhl = "DapBreakpoint" })
+vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint", numhl = "DapBreakpoint" })
+vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint", numhl = "DapLogPoint" })
+vim.fn.sign_define(
+	"DapStopped",
+	{ text = "", texthl = "DapStopped", linehl = "DapStoppedLine", numhl = "DapStopped" }
+)
