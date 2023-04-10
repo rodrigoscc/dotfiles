d75121eb04f2a627be30cb10000a66716456aa80
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2023-04-09 20:12:11 -0500

    Use fzf native in telescope.

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -17,32 +17,68 @@ telescope.setup({
 })
 
 telescope.load_extension("luasnip")
+telescope.load_extension("fzf")
 
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<C-f>", builtin.current_buffer_fuzzy_find)
 
 vim.keymap.set("n", "<leader>/", builtin.live_grep)
 
-vim.keymap.set("n", "<leader>ss", builtin.spell_suggest, { desc = "[s]pell [s]uggest" })
+vim.keymap.set(
+	"n",
+	"<leader>ss",
+	builtin.spell_suggest,
+	{ desc = "[s]pell [s]uggest" }
+)
 
 vim.keymap.set("n", "<leader>ff", function()
 	builtin.find_files({ no_ignore = true })
 end, { desc = "[f]ind [f]iles" })
 
 vim.keymap.set("n", "<leader>ht", builtin.help_tags, { desc = "[h]elp [t]ags" })
-vim.keymap.set("n", "<leader>hk", builtin.keymaps, { desc = "[h]elp [k]eymaps" })
+vim.keymap.set(
+	"n",
+	"<leader>hk",
+	builtin.keymaps,
+	{ desc = "[h]elp [k]eymaps" }
+)
 
 vim.keymap.set("n", "<leader>cc", builtin.commands, { desc = "[c]ommands" })
 
 vim.keymap.set("n", "<leader>rr", builtin.resume, { desc = "[r]esume" })
 
-vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "[g]it [b]ranches" })
-vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "[g]it [c]ommits" })
-vim.keymap.set("n", "<leader>gC", builtin.git_bcommits, { desc = "[g]it buffer [C]ommits" })
+vim.keymap.set(
+	"n",
+	"<leader>gb",
+	builtin.git_branches,
+	{ desc = "[g]it [b]ranches" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>gc",
+	builtin.git_commits,
+	{ desc = "[g]it [c]ommits" }
+)
+vim.keymap.set(
+	"n",
+	"<leader>gC",
+	builtin.git_bcommits,
+	{ desc = "[g]it buffer [C]ommits" }
+)
 
-vim.keymap.set("n", "<leader>ir", builtin.registers, { desc = "[i]nsert [r]egisters" })
+vim.keymap.set(
+	"n",
+	"<leader>ir",
+	builtin.registers,
+	{ desc = "[i]nsert [r]egisters" }
+)
 
-vim.keymap.set("n", "<leader>T", builtin.builtin, { desc = "[T]elescope builtins" })
+vim.keymap.set(
+	"n",
+	"<leader>T",
+	builtin.builtin,
+	{ desc = "[T]elescope builtins" }
+)
 
 vim.keymap.set("n", "<leader>,", function()
 	builtin.buffers({
@@ -51,7 +87,12 @@ vim.keymap.set("n", "<leader>,", function()
 	})
 end, { desc = "buffers" })
 
-vim.keymap.set("n", "<leader>is", "<cmd>Telescope luasnip<cr>", { desc = "[i]nsert [s]nippet" })
+vim.keymap.set(
+	"n",
+	"<leader>is",
+	"<cmd>Telescope luasnip<cr>",
+	{ desc = "[i]nsert [s]nippet" }
+)
 
 local function open_file_at_startup(data)
 	local directory = vim.fn.isdirectory(data.file) == 1
