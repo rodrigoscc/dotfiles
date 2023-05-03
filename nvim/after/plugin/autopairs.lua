123722c571008404ade253a97bcd38604a4b3ef8
Rodrigo Santa Cruz <rodrigo.santacruz@sailinternet.com>
2023-05-03 12:37:14 -0500

    Fix prepend_f_string when cursor is right before the string.

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -12,26 +12,45 @@ local function prepend_f_if_needed()
 	local bufnr = vim.api.nvim_get_current_buf()
 
 	local cursor_node = ts_utils.get_node_at_cursor()
+	if cursor_node == nil then
+		return
+	end
+
 	local node_type = cursor_node:type()
+
+	if node_type ~= "string_content" and node_type ~= "string" then
+		return
+	end
 
 	if node_type == "string_content" then
 		cursor_node = cursor_node:parent()
 		node_type = cursor_node:type()
 	end
 
-	local node_text = vim.treesitter.query.get_node_text(cursor_node, bufnr)
+	local current_win = vim.api.nvim_get_current_win()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(current_win))
+	local node_row, node_col, _ = cursor_node:start()
+	local cursor_is_right_before_the_string = row == node_row + 1
+		and col == node_col
+	if cursor_is_right_before_the_string then
+		return
+	end
+
+	local node_text = vim.treesitter.get_node_text(cursor_node, bufnr)
 
 	local last_three_chars = string.sub(node_text, -3)
 	local first_char = string.sub(node_text, 0, 1)
-	local is_multi_line_str = last_three_chars == "'''" or last_three_chars == '"""'
+	local is_multi_line_str = last_three_chars == "'''"
+		or last_three_chars == '"""'
 
 	if not is_multi_line_str and (first_char == "'" or first_char == '"') then
 		local _, column, _ = cursor_node:start()
 		local current_line = vim.api.nvim_get_current_line()
-		local new_line = current_line:sub(0, column) .. "f" .. current_line:sub(column + 1)
+		local new_line = current_line:sub(0, column)
+			.. "f"
+			.. current_line:sub(column + 1)
 		vim.api.nvim_set_current_line(new_line)
 
-		local current_win = vim.api.nvim_get_current_win()
 		local row, col = unpack(vim.api.nvim_win_get_cursor(current_win))
 		vim.api.nvim_win_set_cursor(current_win, { row, col + 1 })
 	end
