f924b90ef9120ea0de3b29e34882e68cfb4a0aa0
Rodrigo Santa Cruz <rodrigo.santacruz@sailinternet.com>
2023-06-15 10:54:49 -0500

    Add db.Exec to go injections.

--- nvim/after/queries/go/injections.scm
+++ nvim/after/queries/go/injections.scm
@@ -1,6 +1,7 @@
 ; extends
 (call_expression
   (selector_expression) @_function (#any-of? @_function
-                                    "db.Prepare")
+                                    "db.Prepare"
+                                    "db.Exec")
   (argument_list
     . [(raw_string_literal) (interpreted_string_literal)] @sql (#offset! @sql 0 1 0 -1)))
