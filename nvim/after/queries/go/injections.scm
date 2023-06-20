f504c303525a68db5644cf844302131178765fc3
Rodrigo Santa Cruz <rodrigo.santacruz@sailinternet.com>
2023-06-20 14:46:07 -0500

    Improve sql go injection.

--- nvim/after/queries/go/injections.scm
+++ nvim/after/queries/go/injections.scm
@@ -1,7 +1,5 @@
 ; extends
 (call_expression
-  (selector_expression) @_function (#any-of? @_function
-                                    "db.Prepare"
-                                    "db.Exec")
+  (selector_expression) @_function (#match? @_function "(tx|db)\.(Prepare|Exec)")
   (argument_list
     . [(raw_string_literal) (interpreted_string_literal)] @sql (#offset! @sql 0 1 0 -1)))
