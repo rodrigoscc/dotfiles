74b289f3fef187299a3b9ff48fbdd664a4c19ee4
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2023-09-24 23:56:38 -0500

    Change python annotation convention to reST.

--- nvim/lua/custom/packer.lua
+++ nvim/lua/custom/packer.lua
@@ -389,6 +389,13 @@ return require("packer").startup(function(use)
 		config = function()
 			require("neogen").setup({
 				snippet_engine = "luasnip",
+				languages = {
+					python = {
+						template = {
+							annotation_convention = "reST",
+						},
+					},
+				},
 			})
 		end,
 		requires = "nvim-treesitter/nvim-treesitter",
