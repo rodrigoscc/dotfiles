f2fd0e6c9706911082a857d97e56771cbb327b70
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2023-09-25 21:37:34 -0500

    Add vscode rainbow delimiters colors.

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,3 +1,5 @@
+local hl = vim.api.nvim_set_hl
+
 vim.g.colorscheme = "vscode"
 
 vim.cmd([[colorscheme ]] .. vim.g.colorscheme)
@@ -9,4 +11,16 @@ then
 	vim.cmd([[highlight IlluminatedWordText guibg=#333333 gui=none]])
 	vim.cmd([[highlight IlluminatedWordRead guibg=#333333 gui=none]])
 	vim.cmd([[highlight IlluminatedWordWrite guibg=#333333 gui=none]])
+end
+
+if vim.g.colorscheme == "vscode" then
+	local c = require("vscode.colors").get_colors()
+
+	hl(0, "RainbowDelimiterRed", { fg = c.vscLightRed })
+	hl(0, "RainbowDelimiterYellow", { fg = c.vscYellowOrange })
+	hl(0, "RainbowDelimiterBlue", { fg = c.vscDarkBlue })
+	hl(0, "RainbowDelimiterOrange", { fg = c.vscOrange })
+	hl(0, "RainbowDelimiterGreen", { fg = c.vscBlueGreen })
+	hl(0, "RainbowDelimiterViolet", { fg = c.vscViolet })
+	hl(0, "RainbowDelimiterCyan", { fg = c.vscMediumBlue })
 end
