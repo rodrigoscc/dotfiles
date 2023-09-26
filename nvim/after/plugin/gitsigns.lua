2cff75fe90beb9a4bec0d3cf6c9f9d9f46d8de2d
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2023-09-26 17:41:36 -0500

    Clean hydra and neotest usages.

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,5 +1,4 @@
 local gitsigns = require("gitsigns")
-local Hydra = require("hydra")
 
 vim.keymap.set(
 	"n",
@@ -41,77 +40,6 @@ vim.keymap.set("n", "[h", function()
 	vim.g.set_jump(gitsigns.next_hunk, gitsigns.prev_hunk)
 	gitsigns.prev_hunk()
 end, { desc = "previous hunk" })
-
-local hint = [[
-Git stage:
- _J_: next hunk   _S_: stage hunk            _U_: undo stage hunk
- _K_: prev hunk   _P_: preview hunk inline   _R_: reset hunk
- ^
- _q_: exit
-]]
-
-Hydra({
-	name = "Git stage",
-	mode = "n",
-	hint = hint,
-	body = "<leader>G",
-	config = {
-		color = "pink",
-		invoke_on_body = true,
-		hint = {
-			border = "rounded",
-		},
-	},
-	heads = {
-		{
-			"J",
-			function()
-				gitsigns.next_hunk()
-				vim.schedule(function()
-					vim.api.nvim_feedkeys("zz", "n", false)
-				end)
-			end,
-		},
-		{
-			"K",
-			function()
-				gitsigns.prev_hunk()
-				vim.schedule(function()
-					vim.api.nvim_feedkeys("zz", "n", false)
-				end)
-			end,
-		},
-		{
-			"S",
-			function()
-				gitsigns.stage_hunk()
-			end,
-		},
-		{
-			"U",
-			function()
-				gitsigns.undo_stage_hunk()
-			end,
-		},
-		{
-			"R",
-			function()
-				gitsigns.reset_hunk()
-			end,
-		},
-		{
-			"P",
-			function()
-				gitsigns.preview_hunk_inline()
-			end,
-		},
-		{
-			"q",
-			nil,
-			{ exit = true, nowait = true, desc = "exit" },
-		},
-	},
-})
 
 vim.keymap.set("n", "<leader>H", function()
 	gitsigns.setqflist("all")
