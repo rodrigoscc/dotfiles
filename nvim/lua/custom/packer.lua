d9d03ea3ba97e06c9560bcc4af700f0affbe9e2c
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-01-14 18:56:59 -0500

    Install copilot.

--- nvim/lua/custom/packer.lua
+++ nvim/lua/custom/packer.lua
@@ -528,4 +528,16 @@ return require("packer").startup(function(use)
 	})
 
 	use({ "catppuccin/nvim", as = "catppuccin" })
+
+	use({
+		"github/copilot.vim",
+		config = function()
+			vim.keymap.set("i", "<C-d>", 'copilot#Accept("\\<CR>")', {
+				expr = true,
+				replace_keycodes = false,
+			})
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+		end,
+	})
 end)
