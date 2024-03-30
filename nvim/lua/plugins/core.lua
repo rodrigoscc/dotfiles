0057de0f9952b7531ed2192110f54d2fef203a3c
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-03-30 15:17:12 -0500

    Use default obsidian id and metadata.

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -66,24 +66,6 @@ return {
 					path = "~/obsidian-vault/",
 				},
 			},
-			note_id_func = function(title)
-				return title
-			end,
-			note_frontmatter_func = function(note)
-				local out = { id = note.id, tags = note.tags }
-
-				-- `note.metadata` contains any manually added fields in the frontmatter.
-				-- So here we just make sure those fields are kept in the frontmatter.
-				if
-					note.metadata ~= nil and not vim.tbl_isempty(note.metadata)
-				then
-					for k, v in pairs(note.metadata) do
-						out[k] = v
-					end
-				end
-
-				return out
-			end,
 		},
 	},
 }
