12bcb4fa6e82938519e83740045ed4ee8d804b3b
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-03-30 14:02:52 -0500

    Remove auto-list. Add custom highlights for markdown tables.

--- nvim/after/queries/markdown/highlights.scm
+++ nvim/after/queries/markdown/highlights.scm
@@ -1,8 +1,8 @@
 ; extends
-((pipe_table_cell) @my_cell (#lua-match? @my_cell "TODO")) @comment.todo
-((pipe_table_cell) @my_cell (#lua-match? @my_cell "IN PROGRESS")) @comment.hint
-((pipe_table_cell) @my_cell (#lua-match? @my_cell "DONE")) @comment
+((pipe_table_cell) @my_cell (#lua-match? @my_cell "TODO")) @markdown.todo
+((pipe_table_cell) @my_cell (#lua-match? @my_cell "IN PROGRESS")) @markdown.in_progress
+((pipe_table_cell) @my_cell (#lua-match? @my_cell "DONE")) @markdown.done
 
-((pipe_table_cell) @my_cell (#lua-match? @my_cell "HIGH")) @comment.error
-((pipe_table_cell) @my_cell (#lua-match? @my_cell "MEDIUM")) @comment.info
-((pipe_table_cell) @my_cell (#lua-match? @my_cell "LOW")) @comment.note
+((pipe_table_cell) @my_cell (#lua-match? @my_cell "HIGH")) @markdown.high
+((pipe_table_cell) @my_cell (#lua-match? @my_cell "MEDIUM")) @markdown.medium
+((pipe_table_cell) @my_cell (#lua-match? @my_cell "LOW")) @markdown.low
--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
-		priority = 1000,
+		priority = 1500,
 		opts = {
 			sidebars = { "qf", "help", "NeogitCommitMessage" },
 			on_colors = function(colors)
@@ -18,6 +18,36 @@ return {
 			highlight_groups = {
 				MiniTrailspace = {
 					underline = true,
+				},
+				["@markdown.high"] = {
+					bg = "love",
+					fg = "love",
+					blend = 15,
+				},
+				["@markdown.medium"] = {
+					bg = "gold",
+					fg = "gold",
+					blend = 15,
+				},
+				["@markdown.low"] = {
+					bg = "text",
+					fg = "text",
+					blend = 15,
+				},
+				["@markdown.todo"] = {
+					bg = "rose",
+					fg = "rose",
+					blend = 15,
+				},
+				["@markdown.in_progress"] = {
+					bg = "iris",
+					fg = "iris",
+					blend = 15,
+				},
+				["@markdown.done"] = {
+					bg = "muted",
+					fg = "muted",
+					blend = 15,
 				},
 			},
 		},
