d8fd29fea3b011bcd1af91d3b964780f85c11305
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-04-04 23:01:57 -0500

    Include gitcommit filetype in C-s keymap.

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -15,7 +15,7 @@ local neogitGroup = vim.api.nvim_create_augroup("Neogit", { clear = true })
 
 -- Quickly save and quit gitcommit windows.
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "NeogitCommitMessage" },
+	pattern = { "NeogitCommitMessage", "gitcommit" },
 	group = neogitGroup,
 	callback = function()
 		vim.keymap.set("n", "<C-s>", function()
