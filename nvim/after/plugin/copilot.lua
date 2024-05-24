bcc7b8214157cc936b5fb41eab2355b29e2b5e76
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-05-24 16:11:44 -0500

    Silent copilot keymap

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("i", "<C-f>", 'copilot#Accept("\\<CR>")', {
 	expr = true,
 	replace_keycodes = false,
+	silent = true,
 })
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
