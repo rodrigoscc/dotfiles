d9d5b85c151fdef613223359fdb1cbfb2f236347
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-05-28 17:44:20 -0500

    fix: missing http file

---
+++ nvim/after/plugin/http/hooks_utils.lua
@@ -0,0 +1,25 @@
+local project = require("after.plugin.http.project")
+local display = require("after.plugin.http.display")
+local http = require("after.plugin.http.http")
+
+local M = {}
+
+M.update_env = function(override_variables)
+	vim.schedule(function()
+		project.update_active_env(override_variables)
+	end)
+end
+
+M.run_request = function(title, override_variables)
+	vim.schedule(function()
+		http:run_with_title(title, override_variables)
+	end)
+end
+
+M.show = function(response, output)
+	vim.schedule(function()
+		display.show(response, output)
+	end)
+end
+
+return M
