ae1d36b1322210986ab50e0933d7b3105121be00
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-06-20 13:57:20 -0500

    feat: add injection for treesitter queries

--- nvim/after/queries/go/injections.scm
+++ nvim/after/queries/go/injections.scm
@@ -1,10 +1,5 @@
 ; extends
 (call_expression
-  (selector_expression) @_function (#match? @_function "(sqlMock|mock|tx|db)\.(Prepare|Exec|ExpectExec|Query)")
-  (argument_list
-    . [(raw_string_literal) (interpreted_string_literal)] @sql (#offset! @sql 0 1 0 -1)))
-
-(var_declaration
-  (var_spec
-    name: (identifier) @_name (#match? @_name ".*TSQuery")
-    value: (_) @query))
+  (selector_expression (field_identifier) @identifier (#lua-match? @identifier "Query"))
+  (argument_list (interpreted_string_literal) @injection.content (#offset! @injection.content 0 1 0 -1)
+                 (#set! injection.language "query")))
