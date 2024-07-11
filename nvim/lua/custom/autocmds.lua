a5c3cfba0f7f22829d6e31202785c1b617427eab
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-07-10 23:26:05 -0500

    refactor: add augroup function

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -1,6 +1,24 @@
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt.spell = true
-	end,
+local autocmd = vim.api.nvim_create_autocmd
+local groupid = vim.api.nvim_create_augroup
+
+---@param group string
+---@vararg { [1]: string|string[], [2]: vim.api.keyset.create_autocmd }
+---@return nil
+local function augroup(group, ...)
+	local id = groupid(group, {})
+	for _, a in ipairs({ ... }) do
+		a[2].group = id
+		autocmd(unpack(a))
+	end
+end
+
+augroup("Spell", {
+	"FileType",
+	{
+
+		pattern = "markdown",
+		callback = function()
+			vim.opt_local.spell = true
+		end,
+	},
 })
