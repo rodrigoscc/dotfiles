ae831398007475ac29d28d4ca8c272b9338d0ecb
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-10-08 23:48:27 -0500

    chore: update lazy-lock.json and tsserver name

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -233,7 +233,7 @@ require("cmp").setup.filetype({ "sql" }, {
 require("mason").setup({})
 require("mason-lspconfig").setup({
 	ensure_installed = {
-		"tsserver",
+		"ts_ls",
 		"rust_analyzer",
 		"gopls",
 		"basedpyright",
