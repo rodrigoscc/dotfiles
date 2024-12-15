d9450e78c850c6c690403a8bee5a9cdac8749c57
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-12-14 21:23:43 -0500

    chore: tune blink.cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -112,7 +112,6 @@ return {
 			-- elsewhere in your config, without redefining it, via `opts_extend`
 			sources = {
 				providers = {
-					lsp = { fallback_for = { "lazydev" } },
 					lazydev = {
 						name = "LazyDev",
 						module = "lazydev.integrations.blink",
@@ -121,35 +120,14 @@ return {
 					ripgrep = {
 						module = "blink-ripgrep",
 						name = "Ripgrep",
-						-- the options below are optional, some default values are shown
+						max_items = 3,
+						score_offset = -5,
 						---@module "blink-ripgrep"
 						---@type blink-ripgrep.Options
 						opts = {
-							-- For many options, see `rg --help` for an exact description of
-							-- the values that ripgrep expects.
-
-							-- the minimum length of the current word to start searching
-							-- (if the word is shorter than this, the search will not start)
-							prefix_min_len = 3,
-
-							-- The number of lines to show around each match in the preview window.
-							-- For example, 5 means to show 5 lines before, then the match, and
-							-- another 5 lines after the match.
+							prefix_min_len = 5,
 							context_size = 5,
-
-							-- The maximum file size that ripgrep should include in its search.
-							-- Useful when your project contains large files that might cause
-							-- performance issues.
-							-- Examples: "1024" (bytes by default), "200K", "1M", "1G"
 							max_filesize = "1M",
-
-							-- (advanced) Any additional options you want to give to ripgrep.
-							-- See `rg -h` for a list of all available options. Might be
-							-- helpful in adjusting performance in specific situations.
-							-- If you have an idea for a default, please open an issue!
-							--
-							-- Not everything will work (obviously).
-							additional_rg_options = {},
 						},
 					},
 					http = {
@@ -184,6 +162,15 @@ return {
 				documentation = {
 					auto_show = true,
 					auto_show_delay_ms = 200,
+				},
+				menu = {
+					draw = {
+						columns = {
+							{ "kind_icon" },
+							{ "label", "label_description", gap = 1 },
+							{ "source_name" },
+						},
+					},
 				},
 			},
 
