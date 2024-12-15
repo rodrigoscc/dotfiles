21b9636a0102b46aff3ca29f15bcd12de505c445
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-12-15 10:57:39 -0500

    feat: finally update harpoon

--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -20,5 +20,66 @@ return {
 			},
 		},
 	},
-	{ "ThePrimeagen/harpoon" },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		lazy = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{
+				"<leader>a",
+				function()
+					require("harpoon"):list():add()
+				end,
+			},
+			{
+				"<leader>A",
+				function()
+					require("harpoon").ui:toggle_quick_menu(
+						require("harpoon"):list()
+					)
+				end,
+			},
+			{
+				"<M-1>",
+				function()
+					require("harpoon"):list():select(1)
+				end,
+			},
+			{
+				"<M-2>",
+				function()
+					require("harpoon"):list():select(2)
+				end,
+			},
+			{
+				"<M-3>",
+				function()
+					require("harpoon"):list():select(3)
+				end,
+			},
+			{
+				"<M-4>",
+				function()
+					require("harpoon"):list():select(4)
+				end,
+			},
+			{
+				"<M-p>",
+				function()
+					require("harpoon"):list():prev()
+				end,
+			},
+			{
+				"<M-n>",
+				function()
+					require("harpoon"):list():next()
+				end,
+			},
+		},
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+		end,
+	},
 }
