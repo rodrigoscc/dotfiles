2f0b85d61827bafcadd100b6e3e00cd4ff499d22
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2024-12-18 18:55:22 -0500

    feat: run linting even on textchange

--- nvim/lua/plugins/edit.lua
+++ nvim/lua/plugins/edit.lua
@@ -130,7 +130,7 @@ return {
 			}
 
 			vim.api.nvim_create_autocmd(
-				{ "BufWritePost", "BufReadPost", "InsertLeave" },
+				{ "BufWritePost", "BufReadPost", "TextChanged", "InsertLeave" },
 				{
 					callback = function()
 						require("lint").try_lint()
