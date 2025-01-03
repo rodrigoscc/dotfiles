4968cf6f922be9439f1ee6907aad607d543d29ef
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-01-03 16:17:16 -0500

    chore: remove tokyonight colorscheme

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,17 +1,5 @@
 return {
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1500,
-		opts = {
-			sidebars = { "qf", "help", "NeogitCommitMessage" },
-			on_colors = function(colors)
-				-- Border between splits is too dim by default.
-				colors.border = "#565f89"
-			end,
-		},
-	},
-	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		opts = {
