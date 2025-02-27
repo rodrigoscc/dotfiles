5e62b21408f98176da735934e3bea5818ae3e75a
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-02-27 13:13:36 -0500

    feat: add busted and luassert to lazydev

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -71,6 +71,8 @@ return {
 		ft = "lua", -- only load on lua files
 		opts = {
 			library = {
+				{ path = "${3rd}/luassert/library", words = { "assert" } },
+				{ path = "${3rd}/busted/library", words = { "describe" } },
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 				{ path = "snacks.nvim", words = { "Snacks" } },
 			},
