40bd90c934ad8d65e2408f9251d6993bdc9a30f9
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-06-24 22:54:14 -0500

    chore: some updates and no more visual better escape

--- nvim/lua/plugins/better-escape.lua
+++ nvim/lua/plugins/better-escape.lua
@@ -1,6 +1,32 @@
 return {
 	{
 		"max397574/better-escape.nvim",
-		opts = {},
+		opts = {
+			default_mappings = false,
+			mappings = {
+				i = {
+					--  first_key[s]
+					j = {
+						--  second_key[s]
+						k = "<Esc>",
+					},
+				},
+				c = {
+					j = {
+						k = "<C-c>",
+					},
+				},
+				t = {
+					j = {
+						k = "<C-\\><C-n>",
+					},
+				},
+				s = {
+					j = {
+						k = "<Esc>",
+					},
+				},
+			},
+		},
 	},
 }
