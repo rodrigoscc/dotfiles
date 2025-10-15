afce4e5e0646585ab93324fb264cbc78e5bb3ce5
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-10-15 11:28:55 -0500

    feat: add gitcommit snippets

---
+++ nvim/.config/nvim/snippets/gitcommit.lua
@@ -0,0 +1,7 @@
+return {
+	s("fe", fmt([[feat: {}]], { i(1) })),
+	s("fi", fmt([[fix: {}]], { i(1) })),
+	s("ch", fmt([[chore: {}]], { i(1) })),
+	s("pe", fmt([[perf: {}]], { i(1) })),
+	s("do", fmt([[docs: {}]], { i(1) })),
+}
