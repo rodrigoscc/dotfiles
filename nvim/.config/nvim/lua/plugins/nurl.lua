3bc6911a3939ce9711da9e77cc971e832545b6af
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-12-08 16:30:17 -0500

    feat: pass count to R Nurl keymap

--- nvim/.config/nvim/lua/plugins/nurl.lua
+++ nvim/.config/nvim/lua/plugins/nurl.lua
@@ -25,18 +25,15 @@ return {
 			vim.api.nvim_create_autocmd({ "FileType" }, {
 				pattern = "lua",
 				callback = function()
-					vim.keymap.set(
-						"n",
-						"R",
-						"<cmd>Nurl .<cr>",
-						{ buffer = true }
-					)
-					vim.keymap.set(
-						"n",
-						"gR",
-						":Nurl . url[2]=",
-						{ buffer = true }
-					)
+					vim.keymap.set("n", "R", function()
+						if vim.v.count == 0 then
+							vim.cmd("Nurl .")
+						else
+							vim.api.nvim_input(
+								":Nurl . url[" .. vim.v.count .. "]="
+							)
+						end
+					end, { buffer = true })
 				end,
 			})
 		end,
