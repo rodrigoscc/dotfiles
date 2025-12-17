8c758e04e6c63de160ebbc838cee56de1395c518
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-12-16 23:46:58 -0500

    feat: lazy load flash.nvim

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -16,5 +16,35 @@ return {
 				uppercase = false,
 			},
 		},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+			},
+			{
+				"r",
+				mode = { "o" },
+				function()
+					require("flash").treesitter_search()
+				end,
+			},
+			{
+				"<C-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+			},
+		},
 	},
 }
