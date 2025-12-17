ed54446ab2876abef4c7ce6dd16d871c2e1dae6b
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2025-12-16 23:56:16 -0500

    chore: clean after dir

--- nvim/.config/nvim/after/plugin/obsidian.lua
+++ nvim/.config/nvim/after/plugin/obsidian.lua
@@ -45,6 +45,26 @@ vim.keymap.set(
 	{ desc = "[o]bsidian [y]esterday" }
 )
 
+local function new_todo()
+	vim.cmd.Obsidian("today")
+
+	vim.schedule(function()
+		vim.cmd.normal("G")
+		vim.opt.autoindent = false
+		vim.cmd.normal("o")
+		vim.opt.autoindent = true
+		require("checkmate").create()
+	end)
+end
+
+vim.keymap.set(
+	"n",
+	"<leader><space>",
+	new_todo,
+	{ desc = "[o]bsidian new [t]odo" }
+)
+vim.keymap.set("n", "<leader>[", new_todo, { desc = "[o]bsidian new [t]odo" })
+
 vim.keymap.set("n", "<leader>ot", function()
 	Snacks.picker.grep({
 		cwd = "~/obsidian-vault/",
