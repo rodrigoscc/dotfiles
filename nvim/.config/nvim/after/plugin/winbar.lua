48ac608e8044d9b60bdccc95ab2504a38359ea70
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-01-23 00:20:36 -0500

    feat: attach winbar only to needed wins

--- nvim/.config/nvim/after/plugin/winbar.lua
+++ nvim/.config/nvim/after/plugin/winbar.lua
@@ -13,4 +13,16 @@ function _G.Winbar()
 	end
 end
 
-vim.o.winbar = "%{%v:lua.Winbar()%}"
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	group = vim.api.nvim_create_augroup("rodrigoscc/winbar", { clear = true }),
+	callback = function(args)
+		if
+			not vim.api.nvim_win_get_config(0).zindex -- not a floating window
+			and vim.bo[args.buf].buftype == "" -- normal buffer
+			and vim.api.nvim_buf_get_name(args.buf) ~= "" -- has a file name
+			and not vim.wo[0].diff -- not in diff mode
+		then
+			vim.wo.winbar = "%{%v:lua.Winbar()%}"
+		end
+	end,
+})
