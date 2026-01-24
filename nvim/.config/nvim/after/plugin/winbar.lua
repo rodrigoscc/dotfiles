b6f18c079130ee17744c4753169908ea6a1b0591
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-01-24 12:44:37 -0500

    feat: disable winbar in nowrite and nofile

--- nvim/.config/nvim/after/plugin/winbar.lua
+++ nvim/.config/nvim/after/plugin/winbar.lua
@@ -18,6 +18,8 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 	callback = function(args)
 		if
 			not vim.api.nvim_win_get_config(0).zindex -- not a floating window
+			and vim.bo[args.buf].buftype ~= "nowrite"
+			and vim.bo[args.buf].buftype ~= "nofile"
 			and vim.api.nvim_buf_get_name(args.buf) ~= "" -- has a file name
 			and not vim.wo[0].diff -- not in diff mode
 		then
