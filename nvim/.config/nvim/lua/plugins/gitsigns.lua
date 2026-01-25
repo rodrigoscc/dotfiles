f071bae496362731edee63f64cb82e87a2f79b92
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-01-24 19:23:12 -0500

    feat: make sure dap breakpoints are placed after gitsigns

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,6 +4,8 @@ return {
 		opts = {
 			attach_to_untracked = true,
 			signs_staged_enable = false,
+			-- Set a higher priority than dap breakpoint sign priority which is 21
+			sign_priority = 30,
 		},
 	},
 }
