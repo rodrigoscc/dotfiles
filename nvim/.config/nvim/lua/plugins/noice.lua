ef8d358309be95557905be4bb9b99788e4cb6ee9
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-02-04 23:02:24 -0500

    feat: do not want concealing in cmdline

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -4,8 +4,17 @@ return {
 		event = "VeryLazy",
 		opts = {
 			cmdline = {
-				enabled = true, -- enables the Noice cmdline UI
-				view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+				enabled = true,
+				view = "cmdline",
+				format = {
+					cmdline = false,
+					search_down = false,
+					search_up = false,
+					filter = false,
+					lua = false,
+					help = false,
+					input = false,
+				},
 			},
 			lsp = {
 				signature = {
