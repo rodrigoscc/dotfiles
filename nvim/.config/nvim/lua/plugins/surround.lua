0bb7b6ab5f947a9f4d3eb13d0a2e351a30dbdc02
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-02-22 21:14:05 -0500

    feat: remove surround custom keymaps

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -5,16 +5,6 @@ return {
 		event = "VeryLazy",
 		config = function()
 			require("nvim-surround").setup({
-				keymaps = {
-					normal = "gs",
-					normal_cur = "gss",
-					normal_line = "gS",
-					normal_cur_line = "ySS",
-					insert = "<C-g>s",
-					insert_line = "<C-g>S",
-					visual = "gs", -- Remapping visual mode mapping to avoid conflicts with flash.nvim mappings.
-					visual_line = "gS", -- Remapping visual mode mapping to avoid conflicts with flash.nvim mappings.
-				},
 				surrounds = {
 					-- Do not add enclosing whitespace on either character
 					["("] = {
