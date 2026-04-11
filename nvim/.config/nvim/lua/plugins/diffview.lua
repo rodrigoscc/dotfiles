a90901153812745abb853f0c5d2a3ca85c430e7c
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-04-11 01:15:38 -0500

    feat: back to diffview and fugitive

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -3,6 +3,29 @@ return {
 		"sindrets/diffview.nvim",
 		lazy = true,
 		cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewLog" },
+		keys = {
+			{
+				"<leader>gd",
+				function()
+					vim.cmd("DiffviewOpen -- ./") -- Making sure to show only the changes in the current working directory.
+				end,
+				{ desc = "git diff" },
+			},
+			{
+				"<leader>gD",
+				function()
+					vim.cmd("DiffviewOpen origin/HEAD...HEAD") -- Diff current branch to master/main branch in origin
+				end,
+				{ desc = "git diff with origin HEAD" },
+			},
+			{
+				"<leader>gf",
+				function()
+					vim.cmd("DiffviewFileHistory %")
+				end,
+				{ desc = "git file history" },
+			},
+		},
 		config = function()
 			local diffview = require("diffview")
 
