0a576f4c7c229d82dd2aee672dcd91aa1e6197c3
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2026-04-24 15:24:31 -0500

    feat: add subscript surround

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -19,7 +19,38 @@ return {
 					["<"] = {
 						add = { "<", ">" },
 					},
+					["s"] = {
+						add = function()
+							local config = require("nvim-surround.config")
+							local result =
+								config.get_input("Enter the subscript name: ")
+							if result then
+								return { { result .. "[" }, { "]" } }
+							end
+						end,
+						find = function()
+							local config = require("nvim-surround.config")
+
+							return config.get_selection({
+								pattern = "[^=%s%[%]{}]+%b[]",
+							})
+						end,
+						delete = "^(.-%[)().-(%])()$",
+						change = {
+							target = "^(.-%[)().-(%])()$",
+							replacement = function()
+								local config = require("nvim-surround.config")
+								local result = config.get_input(
+									"Enter the subscript name: "
+								)
+								if result then
+									return { { result .. "[" }, { "]" } }
+								end
+							end,
+						},
+					},
 				},
+				aliases = { ["s"] = false },
 			})
 		end,
 	},
